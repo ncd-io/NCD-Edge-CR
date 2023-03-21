9822ced177c0ca33202d198e3a83f0c009193fd9
io-Jacob <jcy.youngblood@gmail.com>
2023-03-21 13:29:59 -0500

    v 0.0.1 logrotate
    Added version and logrotate

--- configuration/scripts/set-node-red-as-service.sh
+++ configuration/scripts/set-node-red-as-service.sh
@@ -13,4 +13,12 @@ pm2 start $nodeRedCommand -- -v
 pm2 save
 pm2 startup systemd
 sudo env PATH=$PATH:/usr/local/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u ncdio --hp /home/ncdio
+
+pm2 install pm2-logrotate
+pm2 set pm2-logrotate:max_size 10M
+pm2 set pm2-logrotate:compress false
+pm2 set pm2-logrotate:retain 14
+pm2 set pm2-logrotate:workerInterval 600
+pm2 set pm2-logrotate:rotateInterval 0 0 * * *
+
 sudo reboot
